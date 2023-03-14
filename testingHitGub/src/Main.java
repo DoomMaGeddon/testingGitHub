a7bd9ed202da90aed2e03cce81251dc2d1010160
DoomMaGeddon <luismi_ale@hotmail.es>
2023-03-14 19:36:03 +0100

    Añadiendo luisMiguel/multiplicar

--- testingHitGub/src/Main.java
+++ testingHitGub/src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(sumar(10, 5));
+        System.out.println(multiplicar(10, 5));
     }
 
     public static int sumar(int x, int y) {
         return x + y;
     }
+    public static int multiplicar(int x, int y) {
+        return x * y;
+    }
+
 }
